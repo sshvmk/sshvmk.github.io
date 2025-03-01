b79e653fae15d9f07c070dc3841e2872f5da466f
Giosuè Sulipano <34242779+imgios@users.noreply.github.com>
2025-03-01 19:47:57 +0100

    Move theme detection function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,1 @@
 console.log('👋🏻 hello from Hugo and imgios!');
-
-{{- if eq $theme "auto" }}
-function detectThemePreferences() {
-    let themeClasses = ["text-dark"];
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        themeClasses = ["text-light", "dark"];
-    }
-    document.body.classList.add(themeClasses);
-}
-
-detectThemePreferences();
-{{- end }}
