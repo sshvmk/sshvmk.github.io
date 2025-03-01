8caed80abbf4bdf582e1c4cd610c7632cc361f9f
Giosuè Sulipano <34242779+imgios@users.noreply.github.com>
2025-03-01 19:39:30 +0100

    Add theme detection and set function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 console.log('👋🏻 hello from Hugo and imgios!');
+
+function detectThemePreferences() {
+    let themeClasses = ["text-dark"];
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        themeClasses = ["text-light", "dark"];
+    }
+    document.body.classList.add(themeClasses);
+}
+
+detectThemePreferences();
